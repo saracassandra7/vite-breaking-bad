ebfd84533e6cf13221605d2399092d8aff2e751e
saracassandra7 <saracassandra7@gmail.com>
2022-11-17 18:51:40 +0100

    [add] select

--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -6,9 +6,20 @@ export default {
 </script>
 
 <template>
+<div class="container mt-5 mb-4">
+  <select class="form-select" aria-label="Default select example">
+  <option selected>Select category</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+</div>
   
 </template>
 
-<style>
+<style lang="scss" scoped>
+.form-select{
+  width: 160px;
+}
 
 </style>
