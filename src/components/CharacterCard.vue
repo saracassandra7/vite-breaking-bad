6aef582da75ab52d72a188e3ce4f53b0a3127000
saracassandra7 <saracassandra7@gmail.com>
2022-11-17 17:36:59 +0100

    [add] characters cards

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  name: 'CharacterCard',
+
+  props: {
+    character: Object,
+  },
+}
+</script>
+
+<template>
+<div class="sc-container pt-5">
+  <div class="sc-card text-center p-3">
+  <img :src="character.img" :alt="character.name">
+  <h6 class="text-white mt-3" >{{character.name}}</h6>
+  <p class="mt-3 mb-0 fs-5">{{character.category}}</p>
+  <p>{{character.status}}</p>
+</div>
+</div>
+  
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/vars' as *;
+
+.sc-container{
+  width: calc(100% / 5);
+  background-color: white;
+
+
+  .sc-card{
+    background-color: $main-color;
+
+   img{
+     max-width: 100%;
+    }
+
+    h6{
+      text-transform: uppercase;
+      font-weight: bold;
+    }
+
+    p{
+      color: #686769;
+    }
+
+
+  }
+}
+
+
+
+</style>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,12 +1,31 @@
 <script>
+import {store} from '../data/store';
+import CharacterCard from './CharacterCard.vue';
+
 export default {
-  name: 'CharacterList'
+  name: 'CharacterList',
+
+  components:{
+    CharacterCard
+  },
+
+  data(){
+    return{
+      store
+    }
+  }
 
 }
 </script>
 
 <template>
-  
+  <div class="container">
+    <div class="row">
+      <CharacterCard v-for="character in store.charactersListData" :key="character.id"
+      :character="character" />
+    </div>
+
+  </div>
 </template>
 
 <style>
