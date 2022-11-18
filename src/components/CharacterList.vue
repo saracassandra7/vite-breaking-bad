eb24588f386b234d7dfd43b32b291de8ea35afd5
saracassandra7 <saracassandra7@gmail.com>
2022-11-18 17:33:16 +0100

    [add] bonus (computed)

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -6,6 +6,15 @@ import AppLoading from './AppLoading.vue';
 export default {
   name: 'CharacterList',
 
+  computed:{
+    outputResult(){
+      if(store.charactersListData.length > 0){
+        return `Found ${store.charactersListData.length} characters`;
+      }
+    }
+
+  },
+
   components:{
     CharacterCard,
     AppLoading
@@ -24,7 +33,7 @@ export default {
   <div class="container bg-white p-5">
     <div class="row">
       <div class="sc-col col text-white pt-3 d-flex align-items-center">
-        <p class="fw-bold fs-5">Found {{store.charactersListData.length}} characters</p>
+        <p class="fw-bold fs-5">{{outputResult}}</p>
       </div>
     </div>
     <div class="row" v-if="store.isLoaded">
